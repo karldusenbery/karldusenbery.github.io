64f580648221a2d65612cdad0f6bf33335afd99c
karldusenbery <karl.dusenbery>
2016-05-10 21:40:15 -0700

    made video responsive like images
    also adjusted scrollTop values for blurb appearing and image changing.

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ $( document ).ready(function() {
 
 $(window).on('scroll', function () {
 	if ($(window).scrollTop() >= 0) {
-		//console.log("you scrolled " + $(window).scrollTop() + " pixels from the top." );
+		console.log("you scrolled " + $(window).scrollTop() + " pixels from the top." );
 		bannerImgTimer = setInterval(function() {
 		    // Add .img-moved-left to first image
 		    $('#bannerImg div:first-of-type').addClass('img-moved-left');
@@ -53,41 +53,48 @@ $(window).on('scroll', function () {
 	}
 });
 
-$(window).on('scroll', function () {
-	if ($(window).scrollTop() >= 320) {
-		console.log("you scrolled greater than 320 pixels from the top." );
+function handler475() {
+	if ($(window).scrollTop() >= 475) {
+		console.log("you scrolled greater than 475 pixels from the top." );
 		$('#taste .left-blurb').css('display', 'block');
 		clearInterval(bannerImgTimer);
 		tasteImgTimer = setInterval(function() {
 		    // Add .img-moved-left to first image
 		    $('#taste .infoImg div:first-of-type').addClass('img-moved-left');
 		}, 5000);
+		// $(window).off('scroll');
 	}
-});
+}
+$(window).on('scroll', handler475);
 
-$(window).on('scroll', function () {
-	if ($(window).scrollTop() >= 995) {
-		console.log("you scrolled greater than 995 pixels from the top." );
+function handler1150() {
+	if ($(window).scrollTop() >= 1150) {
+		console.log("you scrolled greater than 1150 pixels from the top." );
 		$('#sights .right-blurb').css('display', 'block');
 		clearInterval(tasteImgTimer);
 		sightsImgTimer = setInterval(function() {
 		    // Add .img-moved-right to first image
 		    $('#sights .infoImg div:first-of-type').addClass('img-moved-left');
 		}, 5000);
+		// $(window).off('scroll');
 	}
-});
+}
+$(window).on('scroll', handler1150);
 
-$(window).on('scroll', function () {
-	if ($(window).scrollTop() >= 1800) {
-		console.log("you scrolled greater than 1800 pixels from the top." );
+
+function handler1900() {
+	if ($(window).scrollTop() >= 1900) {
+		console.log("you scrolled greater than 1900 pixels from the top." );
 		$('#sounds .left-blurb').css('display', 'block');
 		clearInterval(sightsImgTimer);
 		soundsImgTimer = setInterval(function() {
 		    // Add .img-moved-left to first image
 		    $('#sounds .infoImg div:first-of-type').addClass('img-moved-left');
 		}, 5000);
+		// $(window).off('scroll');
 	}
-});
+}
+$(window).on('scroll', handler1900);
 
 $('.infoImg').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', 'div', function () {
     // append this to end of .infoImg
